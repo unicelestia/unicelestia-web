96aedb8a5fee51347761e0f61233ab1e033b5ff6
lokeshdhakar97 <lokeshdhakar1404@gmail.com>
2024-10-13 19:15:10 +0530

    update sitemap

--- src/app/sitemap.tsx
+++ src/app/sitemap.tsx
@@ -1,11 +1,31 @@
 export default async function Sitemap() {
   return [
     {
-      url: "https://",
+      url: "https://www.unicelestiapharma.com",
       lastModified: new Date()
     },
     {
-      url: "https://${domain}",
+      url: "https://unicelestiapharma.com",
+      lastModified: new Date()
+    },
+    {
+      url: "https://www.unicelestiapharma.com/products/atp-gold-tablets",
+      lastModified: new Date()
+    },
+    {
+      url: "https://www.unicelestiapharma.com/products/rostro-dsr-capsules",
+      lastModified: new Date()
+    },
+    {
+      url: "https://www.unicelestiapharma.com/products/tedflam-sp-tablets",
+      lastModified: new Date()
+    },
+    {
+      url: "https://www.unicelestiapharma.com/products/mirazef-s-injection",
+      lastModified: new Date()
+    },
+    {
+      url: "https://www.unicelestiapharma.com/products/atp-gold-syrup",
       lastModified: new Date()
     }
   ]
